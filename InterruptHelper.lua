5304706e37371f5e7326f66f47337b422494feb9
Arco Dielhof <arcodielhof@hotmail.com>
2021-05-28 21:00:58 +0200

    Version++;

--- InterruptHelper.lua
+++ InterruptHelper.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Interrupt Helper"] = 14
+AZP.VersionControl["Interrupt Helper"] = 15
 if AZP.InterruptHelper == nil then AZP.InterruptHelper = {} end
 
 local AZPIHSelfFrame, AZPInterruptHelperOptionPanel = nil, nil
