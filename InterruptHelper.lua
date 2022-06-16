c264e9f03924422450c07803a8c758e92fe1d3ca
Arco Dielhof <arcodielhof@hotmail.com>
2022-06-16 12:25:32 +0200

    Changed indentation as well as a broken ID.

--- InterruptHelper.lua
+++ InterruptHelper.lua
@@ -1,7 +1,7 @@
 if AZP == nil then AZP = {} end
 if AZP.VersionControl == nil then AZP.VersionControl = {} end
 
-AZP.VersionControl["Interrupt Helper"] = 22
+AZP.VersionControl["Interrupt Helper"] = 23
 if AZP.InterruptHelper == nil then AZP.InterruptHelper = {} end
 if AZP.InterruptHelper.Events == nil then AZP.InterruptHelper.Events = {} end
 
